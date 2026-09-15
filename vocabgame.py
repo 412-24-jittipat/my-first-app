24abc2931934ff54cb89cc38f1b45081dea75af6
412-24-jittipat <54715@yupparaj.ac.th>
2026-09-15 10:50:49 +0700

    Update vocabgame.py

--- vocabgame.py
+++ vocabgame.py
@@ -70,7 +70,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     if score == 4:
         st.success("🎉 You win!")
     else:
-        st.error("💀 you lose")
+        st.error("💀 Bruh")
 
 # ----------------------------------------------------
 # 1. Play Button
@@ -99,17 +99,17 @@ ans1 = st.text_input(
     key="input1"
 )
 ans2 = st.text_input(
-    "ข้อ 2: `b _ _ e` is a piece of skeleton.",
+    "ข้อ 2: Dogs love to eat `b _ _ e`.",
     value=st.session_state.ans2_val,
     key="input2"
 )
 ans3 = st.text_input(
-    "ข้อ 3: The `d _ c _ _ r` is a job to cure patient.",
+    "ข้อ 3: The `d _ c _ _ r` is a job.",
     value=st.session_state.ans3_val,
     key="input3"
 )
 ans4 = st.text_input(
-    "ข้อ 4: Be quiet, the `st _ d _ nt` is learning.",
+    "ข้อ 4:  the `st _ d _ nt` is in school.",
     value=st.session_state.ans4_val,
     key="input4"
 )
@@ -140,63 +140,4 @@ if st.session_state.is_ended:
     )
 
 st.divider()
-st.write("นายกฤตภาส สายทอง ม.4/12 เลขที่ 36")        st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
-
-    # ตรวจข้อ 2
-    if u_ans2 == "fish":
-        st.success("✅ ข้อ 2: ถูกต้อง")
-        score += 1
-    else:
-        st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
-
-    # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
-
-    st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
-
-    if score == 2:
-        st.success("🎉 You win!")
-    else:
-        st.error("💀 You lose!")
-
-
-# ----------------------------------------------------
-# 1. ปุ่มเริ่มเล่นเกม
-# ----------------------------------------------------
-st.button("🎮 เริ่มเล่นเกม", on_click=reset_game)
-
-# 2. แถบแสดงเวลานับถอยหลัง
-if "start" in st.session_state and not st.session_state.get("is_ended", False):
-    time_left = int(30 - (time.time() - st.session_state.start))
-
-    if time_left > 0:
-        st.error(f"⏳ เหลือเวลา: {time_left} วินาที")
-    else:
-        st.session_state.is_ended = True
-        st.rerun()
-
-st.divider()
-
-# 3. ช่องรับคำตอบ (ใช้ value ผูกกับตัวแปรตรงๆ เพื่อสั่งเคลียร์ได้)
-ans1 = st.text_input(
-    "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
-    value=st.session_state.ans1_val,
-)
-ans2 = st.text_input(
-    "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
-    value=st.session_state.ans2_val,
-)
-
-# อัปเดตค่าล่าสุดเข้าตัวแปร
-st.session_state.ans1_val = ans1
-st.session_state.ans2_val = ans2
-
-# ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
-
-
-# 4. ปุ่มส่งคำตอบ
-if "start" in st.session_state and not st.session_state.get("is_ended", False):
-    if st.button("📥 ส่งคำตอบ"):
-        st.session_state.is_ended = True
-        st.rerun()
-
-    time.sleep(1)
+st.write("นายจิตติพัตน์ กาละนิโย ม.4/12 เลขที่ 24")
